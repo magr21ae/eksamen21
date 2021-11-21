4454fbbac985b501db0cffac49e7a75b348ac897
magr21ae <91529047+magr21ae@users.noreply.github.com>
2021-11-21 16:11:46 +0100

    definere user

---
+++ src/models/user.js
@@ -0,0 +1,12 @@
+//Her deffinerer vi hvordan skal se ud når den oprettes på hjemmesiden. 
+class User {
+    constructor(email, password) {
+      this.email = email;
+      this.password = password;
+    }
+  }
+  
+  module.exports = User;
+  
+  //Da man har så få brugere, kan man nøjes med at have en simpel diffinition
+  //På trods af at det er ineffektivt når man skal søge i databasen, generelt ville man arbejde med algoritmer. 
